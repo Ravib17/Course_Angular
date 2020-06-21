6cdf0c94c8be2735c987b946e83916d29f00537a
Ravi Bhatia <ravihbhatia@gmail.com>
2020-06-21 20:11:28 +0530

    animation completed week 4 excercise completed final

--- conFusio/src/app/contact/contact.component.ts
+++ conFusio/src/app/contact/contact.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {FormBuilder,FormGroup,Validator, Validators} from '@angular/forms';
 import {Feedback , ContactType} from '../shared/feedback';
+import { flyInOut } from '../animations/app.animation';
 import { throws } from 'assert';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  animations:[
+   flyInOut()
+  ],
+  host : {
+    '[@flyInOut]':'true',
+     'style':'display:block'
+  }
 })
 export class ContactComponent implements OnInit {
 
